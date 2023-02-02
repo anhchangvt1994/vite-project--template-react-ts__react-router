b8fd1dd5329773c4dfe93558930231fd718ac812
JupiterSamin <anhchangvt1994@gmail.com>
2023-02-03 03:10:14 +0700

    update(other): :jigsaw: install ts-node and sucrase to speed up vite run in window terminal
    install ts-node and sucrase to speed up vite run in window terminal

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from 'path'
 import fs from 'fs'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import alias from '@rollup/plugin-alias'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
